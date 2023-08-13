39f46d5f64de4093d2454c92e7747e1289f9ea9f
tihomir-petrovv <118721@students.ue-varna.bg>
2023-08-13 12:45:49 +0300

    Formatting sidebar sections

--- src/Sidebar/Colors/Colors.js
+++ src/Sidebar/Colors/Colors.js
@@ -1,7 +1,30 @@
 import "./Colors.css";
 
 function Colors() {
-  return <div>Colors</div>;
+  return <div className="ml">
+  <h2 className="sidebar-title color-title">Color</h2>
+  <label className="sidebar-label-container">
+        <input type="radio" name="test2"/>
+        <span className="checkmark"></span>All
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2"/>
+        <span className="checkmark"></span>Black
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2"/>
+        <span className="checkmark"></span>Blue
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2"/>
+        <span className="checkmark"></span>Red
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2"/>
+        <span className="checkmark"></span>Green
+      </label>
+    
+</div>
 }
 
 export default Colors;
