4a31df6205ab408fb40bffcbd060c8203ec46f28
tihomir-petrovv <118721@students.ue-varna.bg>
2023-08-14 15:22:01 +0300

    Adding some functionalities to Sidebar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,70 @@
-import Navigation from "./Navigation/Nav"
+/* eslint-disable array-callback-return */
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import Navigation from "./Navigation/Nav";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
 import Sidebar from "./Sidebar/Sidebar";
+
+import products from "./db/data";
+import Card from "./components/Card"
 function App() {
-  return <>
-  <Sidebar/>
-  <Navigation/>
-  <Recommended/>
-  <Products/>
-  
-  
-  
-  </>;
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const filteredItems = products.filter((product) =>
+    product.title.toLowerCase().indexOf(query.toLowerCase())!== -1);
+
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const handleClick = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  function filteredData(products, selected, query) {
+    let filteredProducts = products;
+
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, price, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          price === selected ||
+          title === selected
+      );
+    }
+
+    return filteredProducts.map(({img, title, price})=>{
+      <Card 
+      key={Math.random()}/>
+      img={img}
+      title={title}
+      price = {price}    
+    })
+  }
+
+  const result = filteredData(products, selectedCategory, query)
+
+  return (
+    <>
+      <Sidebar handleChange={handleChange} />
+      <Navigation />
+      <Recommended />
+      <Products />
+    </>
+  );
 }
 
 export default App;
--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -5,16 +5,16 @@ import "./Sidebar.css"
 import {AiOutlineShoppingCart} from 'react-icons/ai'
 
 
-function Sidebar() {
+function Sidebar({handleChange}) {
   return (
     <>
     <section className="sidebar">
         <div className="logo-container">
             <h1><AiOutlineShoppingCart/></h1>
         </div>
-        <Category/>
-        <Price/>
-        <Colors/>
+        <Category handleChange={handleChange}/>
+        <Price handleChange={handleChange}/>
+        <Colors handleChange={handleChange}/>
     </section>
     </>
   )
